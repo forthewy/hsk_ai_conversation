c420317c79731d9ba8bde2833c54a900afd18797
DESKTOP-J8RJJS7\jane <forthewy@gmail.com>
2026-05-26 22:51:36 +0900

    Reapply "[공통 ui] 하단 ui 구현 #5"
    This reverts commit 0600c4bb0b49a3a415260f551b4bedd19ca7f5a2.

---
+++ lib/screens/game_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build (BuildContext buildContext) {
+    return Scaffold();
+  }
+}
