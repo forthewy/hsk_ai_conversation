d978db57ea2d6f9283de55915b46cd13b09e804d
DESKTOP-J8RJJS7\jane <forthewy@gmail.com>
2026-06-12 16:15:22 +0900

    [Word Screen] 단어장 기능 구현  #4

---
+++ lib/models/word.dart
@@ -0,0 +1,25 @@
+class Word {
+  final String simplified;
+  final String pinyin;
+  final String meanings;
+  final int level;
+
+  Word({
+    required this.simplified,
+    required this.pinyin,
+    required this.meanings,
+    required this.level,
+  });
+
+  factory Word.fromJson(Map<String, dynamic> json,
+      int level) {
+    final form = json['forms'][0];
+
+    return Word(
+      simplified: json['simplified'],
+      pinyin: form['transcriptions']['pinyin'],
+      meanings: (form['meanings'] as List).join(', '),
+      level: level,
+    );
+  }
+}
--- lib/screens/word_screen.dart
+++ lib/screens/word_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:hskchat/screens/start_screen.dart';
+import 'package:flutter/services.dart';
+import '../models/word.dart';
 
 class WordScreen extends StatefulWidget {
   const WordScreen({super.key});
@@ -8,7 +11,12 @@ class WordScreen extends StatefulWidget {
   State<WordScreen> createState() => _WordScreenState();
 }
 
+enum ContentType { wordBook }
+
 class _WordScreenState extends State<WordScreen> {
+  List<Word> allWords = [];
+  ContentType selectedContent = ContentType.wordBook;
+  int selectedHskLevel = 1;
   final wordSearchController = TextEditingController();
 
   @override
@@ -18,7 +26,44 @@ class _WordScreenState extends State<WordScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    loadWords();
+  }
+
+  Future<void> loadWords() async {
+    final files = [
+      'assets/data/1.json',
+      'assets/data/2.json',
+      'assets/data/3.json',
+      'assets/data/4.json',
+      'assets/data/5.json',
+      'assets/data/6.json',
+      'assets/data/7.json',
+    ];
+
+    List<Word> loadedWords = [];
+
+    for (int i = 0; i < files.length; i++) {
+      final level = i + 1;
+      final path = files[i];
+
+      final jsonString = await rootBundle.loadString(path);
+
+      final List<dynamic> jsonList = jsonDecode(jsonString);
+
+      loadedWords.addAll(jsonList.map((e) => Word.fromJson(e, level)));
+    }
+
+    setState(() {
+      allWords = loadedWords;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final words = allWords.where((e) => e.level == selectedHskLevel).toList();
+
     return Scaffold(
       backgroundColor: Color(0xFFB68B74),
       body: SafeArea(
@@ -49,38 +94,104 @@ class _WordScreenState extends State<WordScreen> {
               ),
             ),
             // 사전 종류 (문법.단어.채팅? ETC)
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.sticky_note_2_outlined),
-                  iconSize: 30,
-                ), //인기 단어장
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.sticky_note_2_outlined),
-                  iconSize: 30,
-                ), // 단어장 학습회화
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.sticky_note_2_outlined),
-                  iconSize: 30,
-                ), // HSK 일정
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.sticky_note_2_outlined),
-                  iconSize: 30,
-                ), //
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.sticky_note_2_outlined),
-                  iconSize: 30,
-                ),
-              ],
+            Expanded(
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 50,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        // 단어장 (인기 단어장, 학습회화 등등)
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              selectedContent = ContentType.wordBook;
+                            });
+                          },
+                          icon: Icon(Icons.sticky_note_2_outlined),
+                          iconSize: 30,
+                        ),
+                        // 즐겨찾기
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.star),
+                          iconSize: 30,
+                        ),
+                        // HSK 일정
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.calendar_month),
+                          iconSize: 30,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.sticky_note_2_outlined),
+                          iconSize: 30,
+                        ), //
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.sticky_note_2_outlined),
+                          iconSize: 30,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.grey,
+                      child: Column(
+                        children: [
+                          // HSK 선택
+                          Wrap(
+                            spacing: 8,
+                            children: List.generate(7, (index) {
+                              final level = index + 1;
+
+                              return ChoiceChip(
+                                label: Text('HSK$level'),
+                                selected: selectedHskLevel == level,
+                                onSelected: (_) {
+                                  setState(() {
+                                    selectedHskLevel = level;
+                                  });
+                                },
+                              );
+                            }),
+                          ),
+
+                          const SizedBox(height: 12),
+
+                          // 단어 목록
+                          Expanded(
+                            child: ListView.builder(
+                              itemCount: words.length,
+                              itemBuilder: (context, index) {
+                                final word = words[index];
+
+                                return Card(
+                                  child: ListTile(
+                                    onTap: () {},
+                                    title: Text(word.simplified),
+                                    subtitle: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(word.pinyin),
+                                        Text(word.meanings),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      )
+                    ),
+                  )
+                ],
+              ),
             ),
-            // 검색 결과
-            Container(color: Colors.grey, height: 200),
           ],
         ),
       ),
